860b7e451e63e257c909878578ea44d56695f0b1
LiuTongtong <LiuTongtong7@gmail.com>
2018-10-29 01:17:36 +0800

    2018-10-28: update 3 photos

--- assets/js/images.js
+++ assets/js/images.js
@@ -1,8 +1,10 @@
 var images = [
+    'stones-wudaoying-beijing-258.jpg',
+    'color_love-Forbidden City-beijing-258.jpg',
+    'yard-beichizi-beijing-519.jpg',
     'pepper-haian-jiangsu-461.jpg',
     'ferris_wheel-jiangning-nanjing-260.jpg',
     'microsoft-zhongguancun-beijing-194.jpg',
-    'cell-tsinghua-beijing-146.jpg',
     'desert-dunhuang-gansu-260.jpg',
     'subway-changning-shanghai-346.jpg',
 ];
